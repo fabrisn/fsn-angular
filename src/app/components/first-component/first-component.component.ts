87e8dc77a7b1ea7fc4b1f4b0850255fcb107f921
GALLO\fabri <fabrisn@gmail.com>
2023-08-04 12:54:50 -0300

    Novos components

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.scss']
+})
+export class FirstComponentComponent {
+  name: string = 'Fabri';
+
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+}
---
+++ src/app/components/parent-data/parent-data.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-data',
+  templateUrl: './parent-data.component.html',
+  styleUrls: ['./parent-data.component.scss']
+})
+export class ParentDataComponent {
+  @Input() name: string = '';
+  @Input() userData!: {email: string, role: string};
+ constructor() {}
+
+ ngOnInit(): void {
+
+ }
+}
